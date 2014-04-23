52681827d353c5b3c2cde87b59cdeefb65f7786d
三英電業株式会社　情報システム <jyoho@sanei-dengyo.com>
2014-04-23 13:32:37 +0900

    ok

--- header.php
+++ header.php
@@ -33,9 +33,6 @@ if (function_exists('icl_t')) {
 
 <?php
 	suffusion_document_header();
-	if (is_singular()) {
-		wp_enqueue_script('comment-reply');
-	}
 ?>
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/scripts/html5.js" type="text/javascript"></script>
--- my-shortcode.php
+++ my-shortcode.php
@@ -1,24 +1,19 @@
 <?php
-/**
- * Additional Shortcodes
- */
-
-
 /********************************************************************************/
 /* Google Charts */
 /********************************************************************************/
 function sc_gchart( $atts ) {
-	extract(shortcode_atts(array(
+	extract( shortcode_atts( array(
 			'id'	=>	'gchart_div',
 			'style'	=>	'',
-	), $atts));
+	), $atts ) );
 
 	$styledata = '';
 	if ( $style != '' ) {
-		$styledata = ' style="'.$style.'"';
+		$styledata = ' style="' . $style . '"';
 	}
 
-	return '<div id="'.$id.'"'.$styledata.'></div>';
+	return '<div id="' . $id . '"' . $styledata . '></div>';
 }
 
 add_shortcode( 'gchart', 'sc_gchart' );
@@ -28,22 +23,22 @@ add_shortcode( 'gchart', 'sc_gchart' );
 /* Timeliner */
 /********************************************************************************/
 function sc_timeline( $atts, $content = null ) {
-	extract(shortcode_atts(array(
+	extract( shortcode_atts( array(
 			'style'	=>	'',
-	), $atts));
+	), $atts ) );
 
 	$styledata = '';
 	if ( $style != '' ) {
-		$styledata = ' style="'.$style.'"';
+		$styledata = ' style="' . $style . '"';
 	}
 
 	$content = do_shortcode( $content );
 	$ret  = '';
-	$ret .= '<div id="timelineContainer"'.$styledata.'>';
+	$ret .= '<div id="timelineContainer"' . $styledata . '>';
 	$ret .= '<div class="timelineToggle">';
 	$ret .= '<small><a class="expandAll expandAllclosed"></a></small>';
 	$ret .= '</div>';
-	$ret .= '<br class="clear" />'.$content.'<br class="clear" />';
+	$ret .= '<br class="clear" />' . $content . '<br class="clear" />';
 	$ret .= '</div>';
 	return $ret;
 }
@@ -52,15 +47,15 @@ add_shortcode( 'timeline', 'sc_timeline' );
 
 
 function sc_timeline_group( $atts, $content = null ) {
-	extract(shortcode_atts(array(
+	extract( shortcode_atts( array(
 			'title'	=>	'',
 			'tag'	=>	'h2',
-	), $atts));
+	), $atts ) );
 
 	$content = do_shortcode( $content );
 	$ret  = '';
 	$ret .= '<div class="timelineMajor">';
-	$ret .= '<'.$tag.' class="timelineMajorMarker"><span><a>'.$title.'</a></span></'.$tag.'>';
+	$ret .= '<' . $tag . ' class="timelineMajorMarker"><span><a>' . $title . '</a></span></' . $tag . '>';
 	$ret .= $content;
 	$ret .= '</div>';
 	return $ret;
@@ -70,26 +65,26 @@ add_shortcode( 'timeline_group', 'sc_timeline_group' );
 
 
 function sc_timeline_event( $atts, $content = null ) {
-	extract(shortcode_atts(array(
+	extract( shortcode_atts( array(
 			'id'	=>	'',
 			'title'	=>	'',
 			'event'	=>	'',
 			'tag'	=>	'',
-	), $atts));
+	), $atts ) );
 
 	$eventTag = '';
 	if ( $tag != '' ) {
-		$eventTag = '<'.$tag.'>'.$event.'</'.$tag.'>';
+		$eventTag = '<' . $tag . '>' . $event . '</' . $tag . '>';
 	}
 
 	$content = do_shortcode( $content );
 	$ret  = '';
 	$ret .= '<dl class="timelineMinor">';
-	$ret .= '<dt id="'.$id.'">';
-	$ret .= '<a>'.$title.'</a>';
-	$ret .= '<small id="'.$id.'DT" class="timelineEventDT">'.$event.'</small>';
+	$ret .= '<dt id="' . $id . '">';
+	$ret .= '<a>' . $title . '</a>';
+	$ret .= '<small id="' . $id . 'DT" class="timelineEventDT">' . $event . '</small>';
 	$ret .= '</dt>';
-	$ret .= '<dd id="'.$id.'EX" class="timelineEvent" style="display: none;">';
+	$ret .= '<dd id="' . $id . 'EX" class="timelineEvent" style="display: none;">';
 	$ret .= $eventTag;
 	$ret .= $content;
 	$ret .= '</dd>';
@@ -101,19 +96,19 @@ add_shortcode( 'timeline_event', 'sc_timeline_event' );
 
 
 function sc_timeline_event_single( $atts, $content = null ) {
-	extract(shortcode_atts(array(
+	extract( shortcode_atts( array(
 			'event'	=>	'',
 			'tag'	=>	'h3',
 			'desc'	=>	'',
-	), $atts));
+	), $atts ) );
 
 	$eventTag = '';
 	if ( $tag != '' ) {
-		$eventTag = '<'.$tag.'>'.$event.'</'.$tag.'>';
+		$eventTag = '<' . $tag . '>' . $event . '</' . $tag . '>';
 	}
 	$descTag = '';
 	if ( $desc != '' ) {
-		$descTag = '<p class="desc">'.$desc.'</p>';
+		$descTag = '<p class="desc">' . $desc . '</p>';
 	}
 
 	$content = do_shortcode( $content );
@@ -132,18 +127,18 @@ add_shortcode( 'timeline_event_single', 'sc_timeline_event_single' );
 
 
 function sc_timeline_media( $atts, $content = null ) {
-	extract(shortcode_atts( array(
+	extract( shortcode_atts( array(
 			'style'	=>	'',
-	), $atts));
+	), $atts ) );
 
 	$styledata = '';
 	if ( $style != '' ) {
-		$styledata = ' style="'.$style.'"';
+		$styledata = ' style="' . $style . '"';
 	}
 
 	$content = do_shortcode( $content );
 	$ret  = '';
-	$ret .= '<div class="media"'.$styledata.'>';
+	$ret .= '<div class="media"' . $styledata . '>';
 	$ret .= $content;
 	$ret .= '</div>';
 	return $ret;
@@ -153,30 +148,30 @@ add_shortcode( 'timeline_media', 'sc_timeline_media' );
 
 
 function sc_timeline_gallery( $atts ) {
-	extract(shortcode_atts(array(
+	extract( shortcode_atts( array(
 			'url'	=>	'',
 			'title'	=>	'',
 			'cred'	=>	'',
 			'desc'	=>	'',
 			'rel'	=>	'shadowbox[timeline]',
-	), $atts));
+	), $atts ) );
 
 	$ret = '';
 	if ( $url != '' ) {
 		$credTag = '';
 		if ( $cred != '' ) {
-			$credTag = '<p class="cred"><small>'.$cred.'</small></p>';
+			$credTag = '<p class="cred"><small>' . $cred . '</small></p>';
 		}
 		$descTag = '';
 		if ( $desc != '' ) {
-			$descTag = '<p class="desc">'.$desc.'</p>';
+			$descTag = '<p class="desc">' . $desc . '</p>';
 		}
 
 		$ret = '';
 		$ret .= '<div class="gallery default">';
 		$ret .= '<div class="gallery-icon">';
-		$ret .= '<a href="'.$url.'" rel="'.$rel.'">';
-		$ret .= '<img title="'.$title.'" src="'.$url.'" alt="" />';
+		$ret .= '<a href="' . $url . '" rel="' . $rel . '">';
+		$ret .= '<img title="' . $title . '" src="' . $url . '" alt="" />';
 		$ret .= '</a>';
 		$ret .= '</div>';
 		$ret .= '</div>';
@@ -190,18 +185,18 @@ add_shortcode( 'timeline_gallery', 'sc_timeline_gallery' );
 
 
 function sc_timeline_embed( $atts, $content = null ) {
-	extract(shortcode_atts(array(
+	extract( shortcode_atts( array(
 			'cred'	=>	'',
 			'desc'	=>	'',
-	), $atts));
+	), $atts ) );
 
 	$credTag = '';
 	if ( $cred != '' ) {
-		$credTag = '<p class="cred"><small>'.$cred.'</small></p>';
+		$credTag = '<p class="cred"><small>' . $cred . '</small></p>';
 	}
 	$descTag = '';
 	if ( $desc != '' ) {
-		$descTag = '<p class="desc">'.$desc.'</p>';
+		$descTag = '<p class="desc">' . $desc . '</p>';
 	}
 
 	$content = do_shortcode( $content );
@@ -218,17 +213,17 @@ add_shortcode( 'timeline_embed', 'sc_timeline_embed' );
 
 
 function sc_timeline_infolist( $atts ) {
-	extract(shortcode_atts(array(
+	extract( shortcode_atts( array(
 			'list'	=>	'',
 			'delim'	=>	'::::',
-	), $atts));
+	), $atts ) );
 
 	$liTag = '';
 	if ( $list != '' ) {
 		$array = explode( $delim, $list );
 		$count = count( $array );
 		for ( $i = 0; $i < $count; $i++ ) {
-			$liTag .= '<li>'.$array[$i].'</li>';
+			$liTag .= '<li>' . $array[ $i ] . '</li>';
 		}
 	}
 
@@ -246,18 +241,18 @@ add_shortcode( 'timeline_infolist', 'sc_timeline_infolist' );
 /* Google Maps Column */
 /********************************************************************************/
 function sc_map_column( $atts, $content = null ) {
-	extract(shortcode_atts(array(
+	extract( shortcode_atts( array(
 			'style'	=>	'',
-	), $atts));
+	), $atts ) );
 
 	$styledata = '';
 	if ( $style != '' ) {
-		$styledata = ' style="'.$style.'"';
+		$styledata = ' style="' . $style . '"';
 	}
 
 	$content = do_shortcode( $content );
 	$ret  = '';
-	$ret .= '<div id="mapcontainer"'.$styledata.'>';
+	$ret .= '<div id="mapcontainer"' . $styledata . '>';
 	$ret .= $content;
 	$ret .= '</div>';
 	return $ret;
@@ -267,18 +262,18 @@ add_shortcode( 'map_column', 'sc_map_column' );
 
 
 function sc_map_location( $atts, $content = null ) {
-	extract(shortcode_atts(array(
+	extract( shortcode_atts( array(
 			'style'	=>	'',
-	), $atts));
+	), $atts ) );
 
 	$styledata = '';
 	if ( $style != '' ) {
-		$styledata = ' style="'.$style.'"';
+		$styledata = ' style="' . $style . '"';
 	}
 
 	$content = do_shortcode( $content );
 	$ret  = '';
-	$ret .= '<div id="addrcolumn"'.$styledata.'>';
+	$ret .= '<div id="addrcolumn"' . $styledata . '>';
 	$ret .= $content;
 	$ret .= '</div>';
 	return $ret;
@@ -288,23 +283,23 @@ add_shortcode( 'map_location', 'sc_map_location' );
 
 
 function sc_map_frame( $atts, $content = null ) {
-	extract(shortcode_atts(array(
+	extract( shortcode_atts( array(
 			'style'	=>	'',
 			'src'	=>	'',
 			'width'	=>	'320',
-	), $atts));
+	), $atts ) );
 
 	$styledata = '';
 	if ( $style != '' ) {
-		$styledata = ' style="'.$style.'"';
+		$styledata = ' style="' . $style . '"';
 	}
 
 	$content = do_shortcode( $content );
 	$ret  = '';
-	$ret .= '<div id="mapcolumn"'.$styledata.'>';
+	$ret .= '<div id="mapcolumn"' . $styledata . '>';
 	$ret .= '<div id="mapiframe">';
 	$ret .= $content;
-	$content = do_shortcode( '[map_iframe src="'.$src.'" width="'.$width.'"]' );
+	$content = do_shortcode( '[map_iframe src="' . $src . '" width="' . $width . '"]' );
 	$ret .= $content;
 	$ret .= '</div>';
 	$ret .= '</div>';
@@ -315,13 +310,13 @@ add_shortcode( 'map_frame', 'sc_map_frame' );
 
 
 function sc_map_iframe( $atts ) {
-	extract(shortcode_atts(array(
+	extract( shortcode_atts( array(
 			'src'	=>	'',
 			'width'	=>	'320',
-	), $atts));
+	), $atts ) );
 
 	$ret  = '';
-	$ret .= '<iframe name="mapiframe1" src="'.$src.'" width="'.$width.'"></iframe>';
+	$ret .= '<iframe name="mapiframe1" src="' . $src . '" width="' . $width . '"></iframe>';
 	return $ret;
 }
 
@@ -329,27 +324,27 @@ add_shortcode( 'map_iframe', 'sc_map_iframe' );
 
 
 function sc_map_link( $atts ) {
-	extract(shortcode_atts(array(
-			'title' => '住所',
-			'small' => 'true',
-			'ellipsis' => 'true',
-			'href' => '',
-			'name' => 'globe',
-			'desc' => '大きな地図で見る',
-	), $atts));
+	extract( shortcode_atts( array(
+			'title'		=>	'住所',
+			'small'		=>	'true',
+			'ellipsis'	=>	'true',
+			'href'		=>	'',
+			'name'		=>	'globe',
+			'desc'		=>	'大きな地図で見る',
+	), $atts ) );
 
-	$content = do_shortcode( '[iconfont name="'.$name.'"]' );
+	$content = do_shortcode( '[iconfont name="' . $name . '"]' );
 	$ret  = '';
 	if ( $title != '' ) {
-		$ret .= '<span id="maptitle">'.$title.'</span>';
+		$ret .= '<span id="maptitle">' . $title . '</span>';
 	}
 	if ( $ellipsis == 'true' ) {
-		$ret .= '　<sub>'.do_shortcode( '[iconfont name="ellipsis-h"]' ).'</sub>';
+		$ret .= '　<sub>' . do_shortcode( '[iconfont name="ellipsis-h"]' ) . '</sub>';
 	}
 	if ( $small == 'true' ) {
 		$ret .= '<small>';
 	}
-	$ret .= '<a class="largermap ext-link ext-icon-2" href="'.$href.'" rel="external nofollow" target="_blank">';
+	$ret .= '<a class="largermap ext-link ext-icon-2" href="' . $href . '" rel="external nofollow" target="_blank">';
 	$ret .= $content;
 	$ret .= $desc;
 	$ret .= '</a>';
@@ -360,4 +355,3 @@ function sc_map_link( $atts ) {
 }
 
 add_shortcode( 'map_link', 'sc_map_link' );
-?>
